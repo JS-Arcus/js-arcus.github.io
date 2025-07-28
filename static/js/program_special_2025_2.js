54bde47477b3853aa04b7307460b71e4e7341ed2
sossinayDev <125735344+sossinayDev@users.noreply.github.com>
2025-07-28 11:59:24 +0200

    changed file download names

--- static/js/program_special_2025_2.js
+++ static/js/program_special_2025_2.js
@@ -25,19 +25,22 @@ window.addEventListener('DOMContentLoaded', () => {
     update_color()
 });
 
-function download_file(file_name) {
+function download_file(file_name, display) {
     const link = document.createElement('a');
     link.href = `static/${file_name}`;
     link.download = file_name;
+    if (display) {
+        link.download = display;
+    }
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
 }
 
 function download_ical() {
-    download_file("semesters/2025-2/ical.ics")
+    download_file("semesters/2025-2/ical.ics", "js_arcus_termine_levelup.ics")
 }
 
 function download_pdf() {
-    download_file("semesters/2025-2/print.pdf")
+    download_file("semesters/2025-2/print.pdf", "js_arcus_semesterprogramm_levelup.pdf")
 }

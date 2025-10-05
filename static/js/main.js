725c268284e9e4469ab67b23d0a4e440aeee6de2
sossinayDev <arcusteamkalender@gmail.com>
2025-10-05 12:02:36 +0200

    Hela-Fotos, neuer Standort

--- static/js/main.js
+++ static/js/main.js
@@ -17,7 +17,25 @@ function handle_obfuscation(){
 }
 
 let obfuscation_interval = null
+let map = null
+
 
 window.onload = ()=>{
     obfuscation_interval = setInterval(handle_obfuscation, 10)
+
+    map = L.map('location_map').setView([46.940629514276154, 7.392745334483644], 17);
+    // Add OSM tiles
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OSM</a>'
+    }).addTo(map);
+    // Add marker
+    const myIcon = L.icon({
+        iconUrl: 'https://js-arcus.github.io/static/img/js_arcus_logo.png',
+        iconSize: [25, 25], // size of the icon
+        iconAnchor: [15, 15], // point of the icon which corresponds to the marker's location
+        popupAnchor: [0, -13], // point where the popup opens relative to the iconAnchor
+    });
+    L.marker([46.94069910339558, 7.392667550419796], { icon: myIcon }).addTo(map)
+    map.invalidateSize();
 }

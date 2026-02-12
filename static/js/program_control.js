fb74b9c07a27f9283edd58aec8af125439c6cdee
JS-Arcus <210595467+JS-Arcus@users.noreply.github.com>
2026-02-12 11:07:24 +0100

    Foto-Library update und Programmwechsel
    - program.html leitet nun zum Programm 2026-1 weiter
    - Foto-Library enthält nun Fotos zum Schliwo 26
    - Foto-Library enthält nun Previews, welche schneller geladen werden können

--- static/js/program_control.js
+++ static/js/program_control.js
@@ -1,8 +1,8 @@
 function redirect_to_program(){
     let spid = document.location.hash.replaceAll("#","");
     console.log(spid);
-    if (spid=="") {spid="2025-2"}
-    if (spid=="next") {spid="2026-1"}
+    if (spid=="") {spid="2026-1"}
+    if (spid=="next") {spid="2026-2"}
     let a = document.createElement("a");
     a.href = `program_${spid}.html`;
     a.click();
